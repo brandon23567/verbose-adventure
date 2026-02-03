e19ea0fbc2e8b0a574e89f753a13c145b31220f7
brandon23567 <brandonmahuvava0@gmail.com>
2026-02-04 00:22:31 +0530

    added changes to the main.py file to accomodate the new url for the frontend to connect to along with cors

--- app/main.py
+++ app/main.py
@@ -12,6 +12,22 @@ app = FastAPI(
     version="1.0.0",
 )
 
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://verbose-adventure-uau8.onrender.com"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(task.router)
 
 @app.get("/")
